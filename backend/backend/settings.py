e29ff47365ed5aa9ae36567cf1297bdbefd2ff70
petrovkrs <petrovkrs@yandex.ru>
2024-10-12 12:56:34 +0300

    Dockerfile зависимости для postgres

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,7 +96,7 @@ if DB_TYPE == 'postgres':
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.getenv('POSTGRES_DB', 'django'),
             'USER': os.getenv('POSTGRES_USER', 'django'),
-            'PASSWORD': os.getenv('POSTGRES_PASSWORD', '123'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
             'HOST': os.getenv('DB_HOST', ''),
             'PORT': os.getenv('DB_PORT', 5432)
         }
