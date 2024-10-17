e59f6c58726240bd1a22fc28713af64375b9ab9c
petrovkrs <petrovkrs@yandex.ru>
2024-10-17 21:55:19 +0300

    in admin

--- frontend/src/shared/ui/Subtitle/Subtitle.tsx
+++
@@ -1,9 +0,0 @@
-import 'src/shared/ui/Subtitle/Subtitle.scss';
-
-interface SubtitleProps {
-  text: string;
-}
-
-export default function Subtitle({ text }: SubtitleProps) {
-  return <h2 className='subtitle'>{text}</h2>;
-}
