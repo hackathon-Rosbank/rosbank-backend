36fbf3a709c874741ea0893ad7da73fbe028937e
petrovkrs <petrovkrs@yandex.ru>
2024-10-18 02:30:08 +0300

    frontend

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,64 +1,32 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework import routers
 
 from api.views import (
-    MetricViewSet,
-    EmployeesViewSet,
-    TeamMetricViewSet,
-    TeamCountEmployeeViewSet,
+    MetricViewSet, EmployeesViewSet,
+    TeamMetricViewSet, TeamCountEmployeeViewSet,
     TeamIndividualCompetenciesViewSet,
-    TeamIndividualSkillsViewSet,
     CompetencyLevelViewSet,
-    SkillLevelViewSet
+    SkillLevelViewSet,
 )
 
+from .views import DevelopmentPlan, TeamIndividualSkillsViewSet
+from core.models import AssesmentSkill
 
 router_v1 = routers.DefaultRouter()
 
-router_v1.register(
-    r'teams/(?P<team_slug>[\w-]+)/count_employees',
-    TeamCountEmployeeViewSet,
-    basename='count_employees',
-)
-router_v1.register(
-    r'teams/(?P<team_slug>[\w-]+)/employees',
-    EmployeesViewSet,
-    basename='employees',
-)
-router_v1.register(
-    r'metrics/(?P<metric_type>development_plan|involvement)/(?P<employee_id>\d+)',
-    MetricViewSet,
-    basename='metric',
-)
 
-router_v1.register(
-    r'teams/(?P<team_slug>[\w-]+)/(?P<metric_type>development_plan|involvement)',
-    TeamMetricViewSet,
-    basename='team_metric',
-)
-
-
-router_v1.register(
-    r'teams/(?P<team_slug>[\w-]+)/individual_competencies(?:/(?P<employee_id>\d+))?',
-    TeamIndividualCompetenciesViewSet,
-    basename='individual_competencies',
-)
-
+router_v1.register(r'teams/(?P<team_slug>[\w-]+)/count_employees', TeamCountEmployeeViewSet, basename='count_employees')
+router_v1.register(r'teams/(?P<team_slug>[\w-]+)/employees', EmployeesViewSet, basename='employees')
+router_v1.register(r'metrics/(?P<metric_type>development_plan|involvement)/(?P<employee_id>\d+)', MetricViewSet, basename='metric')
+router_v1.register(r'teams/(?P<team_slug>[\w-]+)/(?P<metric_type>development_plan|involvement)', TeamMetricViewSet, basename='team_metric') # План развития вовлеченность команды
+router_v1.register(r'teams/(?P<team_slug>[\w-]+)/individual_competencies(?:/(?P<employee_id>\d+))?', TeamIndividualCompetenciesViewSet, basename='individual_competencies')
 router_v1.register(
     r'teams/(?P<team_slug>[\w-]+)/competencies_level(?:/(?P<employee_id>\d+))?',
     CompetencyLevelViewSet,
     basename='competency_level'
 )
-router_v1.register(
-    r'teams/(?P<team_slug>[\w-]+)/skills(?:/(?P<employee_id>\d+))?',
-    TeamIndividualSkillsViewSet,
-    basename='skills'
-)
-router_v1.register(
-    r'teams/(?P<team_slug>[\w-]+)/skills/level',
-    SkillLevelViewSet,
-    basename='skill-level'
-)
+router_v1.register(r'teams/(?P<team_slug>[\w-]+)/skills(?:/(?P<employee_id>\d+))?', TeamIndividualSkillsViewSet, basename='skills')
+router_v1.register(r'teams/(?P<team_slug>[\w-]+)/skills/level', SkillLevelViewSet, basename='skill-level')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
