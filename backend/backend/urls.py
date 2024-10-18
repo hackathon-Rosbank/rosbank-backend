388f62572abe1837fcf6dd143699df051c892d05
petrovkrs <petrovkrs@yandex.ru>
2024-10-19 00:10:04 +0300

    fron OK

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,3 +1,19 @@
+"""
+URL configuration for backend project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -8,28 +24,31 @@ from django.urls import path, include
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="API Documentation",
+        title="API Documentation R-Bank",
         default_version='v1',
-        description="API documentation for the project",
+        description="Документация для проекта Ros-Bank",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="your-email@example.com"),
         license=openapi.License(name="BSD License"),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
-    url="https://rosb-hakaton.ddns.net",
-    # schemes=['https'],  # Ограничьте схемы только https
 )
-
-
 
 
 urlpatterns = (
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
+    path(
+        'swagger/',
+        schema_view.with_ui('swagger', cache_timeout=5),
+        name='schema-swagger-ui',
+    ),
+    path(
+        'redoc/',
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc',
+    ),
     # path('auth/', include('djoser.urls')),
     # # JWT-эндпоинты, для управления JWT-токенами:
     # path('auth/', include('djoser.urls.authtoken')),       # JWT-эндпоинты
